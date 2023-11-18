ba9456f7fadca5961b3f86720ee21d4fac248253
Zercopral <80348366+Zercopral@users.noreply.github.com>
2023-11-18 14:27:30 +0300

    Initial commit

---
+++ code/test.py
@@ -0,0 +1,12 @@
+# a = 5
+# b = 10
+# (a, b) = (b, a)
+# print('a = ', a, ' b = ', b)
+
+# def terurn_two():
+#     return (" а мне нравится ", " спорить? ")
+
+# a, b = terurn_two()
+# print(b, a)
+
+assert 5 == 6
